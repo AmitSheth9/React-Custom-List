6af85aabeaf54f6a2e7ad11af361ed82c7895bba
AmitSheth9 <amit.sheth9@gmail.com>
2022-01-11 16:24:20 -0600

    components working

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { move } from 'superagent'
 
 export default function Detail({movie}) {
     return (
@@ -6,8 +7,13 @@ export default function Detail({movie}) {
             <p>{movie.title}</p>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
             <p>{movie.overview}</p>
+            <p>Rating: {movie.vote_average}</p>
             <p>Budget: {movie.budget}</p>
-            <p></p>
+            <p>Revenue: {movie.revenue}</p>
+            <p>Release date: {movie.release_date}</p>
+            <p>Runtime: {movie.runtime}</p>
+            <p>Status: {movie.status}</p>
+            <p>Tagline: {movie.tagline}</p>
         </div>
     )
 }
--- src/services/fetch-movies.jsx
+++ src/services/fetch-movies.jsx
@@ -2,7 +2,7 @@ import request from "superagent";
 
 const popularURL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.apikey}&language=en-US&page=1`
 
-const movieURL = `http://api.themoviedb.org/3/movie/634649?api_key=5787ecc0ae092dc17128bfee5aebeb24`
+
 
 export async function getPopularMovies() {
     const response = await request
@@ -12,6 +12,6 @@ export async function getPopularMovies() {
 
 export async function getByID(id) {
     const response = await request
-    .get(`http://api.themoviedb.org/3/movie/${id}?api_key=5787ecc0ae092dc17128bfee5aebeb24`);
+    .get(`http://api.themoviedb.org/3/movie/${id}?api_key=${process.env.apikey}`);
     return response;
 }
